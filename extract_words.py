1a01db31c213393d7e4bfeeba6d170aa20a25d22
nfeldt2 <n.feldt@utexas.edu>
2023-09-13 14:49:17 -0500

    initial commit with text files and extract_words.py

---
+++ extract_words.py
@@ -0,0 +1,52 @@
+import numpy as np
+import re
+
+
+def all_words(words):
+    allwords = ' '.join(words)
+
+    file_path_all = 'C:\\Users\\Nathan\\allwords.txt'
+    with open(file_path_all, 'w', encoding='utf-8') as f:
+        f.write(allwords)
+
+def unique_words(words):
+    unique, counts = np.unique(words, return_counts=True) # returns unique words and their counts
+
+    unique = ' '.join(unique)
+    file_path_unique = 'C:\\Users\\Nathan\\uniquewords.txt'
+    with open(file_path_unique, 'w', encoding='utf-8') as f:
+        f.write(unique)
+
+    return counts
+
+def word_frequency(counts):
+    counts, countsOfCounts = np.unique(counts, return_counts=True) # returns the counts of the counts (e.g. a count of 1 occurs 1000 times)
+
+    frequencies = dict(zip(counts, countsOfCounts))
+
+    file_path_freq = 'C:\\Users\\Nathan\\wordfrequency.txt'
+    with open(file_path_freq, 'w', encoding='utf-8') as f:
+        for key, value in frequencies.items():
+            f.write(f"{key}: {value}\n")
+
+def main():
+    with open('SunAlsoRises.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    words = [re.sub(r'[—.,‘\'"“”!?()\[\]{}:;_/0123456789#$%=&•*-]', '', word) for word in text.split()]
+
+    words = np.array(words)
+
+    words = np.char.lower(words)
+
+    words = np.sort(words)
+
+    all_words(words)
+
+    counts = unique_words(words)
+
+    word_frequency(counts)
+
+
+if __name__ == '__main__':
+    main()
