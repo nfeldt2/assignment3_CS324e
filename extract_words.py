a90a5ceadfa2c9106dd6fbaa51489b5827a37c70
nfeldt2 <n.feldt@utexas.edu>
2023-09-13 15:08:24 -0500

    made changes to extract_words.py

--- extract_words.py
+++ extract_words.py
@@ -5,6 +5,7 @@ import re
 def all_words(words):
     allwords = ' '.join(words)
 
+    # write all words to a file
     file_path_all = 'C:\\Users\\Nathan\\allwords.txt'
     with open(file_path_all, 'w', encoding='utf-8') as f:
         f.write(allwords)
@@ -12,6 +13,7 @@ def all_words(words):
 def unique_words(words):
     unique, counts = np.unique(words, return_counts=True) # returns unique words and their counts
 
+    # write all unique words to a file
     unique = ' '.join(unique)
     file_path_unique = 'C:\\Users\\Nathan\\uniquewords.txt'
     with open(file_path_unique, 'w', encoding='utf-8') as f:
@@ -22,26 +24,29 @@ def unique_words(words):
 def word_frequency(counts):
     counts, countsOfCounts = np.unique(counts, return_counts=True) # returns the counts of the counts (e.g. a count of 1 occurs 1000 times)
 
+    # convert to dicti
     frequencies = dict(zip(counts, countsOfCounts))
 
+    # write word frequencies to a file
     file_path_freq = 'C:\\Users\\Nathan\\wordfrequency.txt'
     with open(file_path_freq, 'w', encoding='utf-8') as f:
         for key, value in frequencies.items():
             f.write(f"{key}: {value}\n")
 
 def main():
+    # read in text file
     with open('SunAlsoRises.txt', 'r', encoding='utf-8') as f:
         text = f.read()
 
-    words = [re.sub(r'[—.,‘\'"“”!?()\[\]{}:;_/0123456789#$%=&•*-]', '', word) for word in text.split()]
+    words = [re.sub(r'[—.,‘\'"“”!?()\[\]{}:;_/0123456789#$%=&•*-]', '', word) for word in text.split()] # remove unwanted characters (used regex library because strip only removes from the ends of the string)
 
     words = np.array(words)
 
-    words = np.char.lower(words)
+    words = np.char.lower(words) # convert all words to lowercase
 
-    words = np.sort(words)
+    words = np.sort(words) # for simplicity
 
-    all_words(words)
+    all_words(words) 
 
     counts = unique_words(words)
 
